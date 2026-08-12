b3f735a2ae239c42bb561661439ca341564bdc6f
Gilles VLAVONOU <deboradoumatey@gmail.com>
2026-08-12 14:46:22 +0100

    Création des endpoints pour l'authentification

---
+++ app/Lib/Endpoint.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Lib;
+
+class Endpoint
+{
+    public const CONNEXION = 'connexion';
+    public const MOT_DE_PASSE_OUBLIE = 'mot-de-passe-oublie';
+    public const VERIFIER_OTP = 'verifier-otp';
+    public const REINITIALISER_MOT_DE_PASSE = 'reinitialiser-mot-de-passe';
+}
