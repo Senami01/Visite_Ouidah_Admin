98a5dfc9b6784746e621bd62810c38e4af2e8818
Gilles VLAVONOU <deboradoumatey@gmail.com>
2026-08-12 16:20:22 +0100

    Fonction de régénération de mot de passe

--- app/Lib/Helper.php
+++ app/Lib/Helper.php
@@ -37,3 +37,35 @@ if (! function_exists('reinitialiser_mot_de_passe')) {
         ]);
     }
 }
+
+if (! function_exists('regenerer_mot_de_passe')) {
+    function regenerer_mot_de_passe(User $utilisateur): string
+    {
+        $majuscule = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $minuscule = 'abcdefghijklmnopqrstuvwxyz';
+        $chiffre = '0123456789';
+        $special = '@#$';
+
+        $pass[] = $majuscule[rand(0, strlen($majuscule) - 1)];
+        $pass[] = $minuscule[rand(0, strlen($minuscule) - 1)];
+        $pass[] = $chiffre[rand(0, strlen($chiffre) - 1)];
+        $pass[] = $special[rand(0, strlen($special) - 1)];
+
+        $Caracteres = $majuscule . $minuscule . $chiffre . $special;
+        for ($i = 0; $i < 4; $i++) {
+            $pass[] = $Caracteres[rand(0, strlen($Caracteres) - 1)];
+        }
+
+        shuffle($pass);
+        $motDePasseClair = implode('', $pass);
+
+        $utilisateur->update([
+            FieldName::PASSWORD => Hash::make($motDePasseClair),
+            FieldName::OTP => null,
+            FieldName::EXPIRE_LE => null,
+            FieldName::EMAIL_VERIFIE_LE => now(),
+        ]);
+
+        return $motDePasseClair;
+    }
+}
