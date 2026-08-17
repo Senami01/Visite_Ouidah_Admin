24d2816349150a4cb2f7ecfbc8bd496241542643
Gilles VLAVONOU <deboradoumatey@gmail.com>
2026-08-17 15:47:44 +0100

    mise à jour des endpoints

--- app/Lib/Endpoint.php
+++ app/Lib/Endpoint.php
@@ -6,6 +6,8 @@ class Endpoint
 {
     public const CONNEXION = 'connexion';
     public const MOT_DE_PASSE_OUBLIE = 'mot-de-passe-oublie';
-    public const VERIFIER_OTP = 'verifier-otp';
+    public const ROLES = 'roles';
     public const REINITIALISER_MOT_DE_PASSE = 'reinitialiser-mot-de-passe';
+    public const ADMINISTRATEURS = 'administrateurs';
+    public const VERIFIER_OTP = 'verifier-otp';
 }
