6c38381e25e715932373db6b0b26446e53b9ae15
Gilles VLAVONOU <deboradoumatey@gmail.com>
2026-08-18 15:08:38 +0100

    helper mise à jour

--- app/Lib/Helper.php
+++ app/Lib/Helper.php
@@ -1,34 +1,39 @@
 <?php
 
+namespace App\Lib;
+
 use App\Models\User;
 use App\Lib\FieldName;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-function generer_jeton_connexion(User $utilisateur, string $nomJeton = 'token'): string
+class Helper
 {
-    return $utilisateur->createToken($nomJeton)->accessToken;
-}
+   public static function generer_jeton_connexion(User $utilisateur, string $nomJeton = 'token'): string
+    {
+        return $utilisateur->createToken($nomJeton)->accessToken;
+    }
 
-function generer_otp(User $utilisateur, int $minutesValidite = 10): int
-{
-    $otpBrut = random_int(100000, 999999);
-    $utilisateur->update([
-        FieldName::OTP => Hash::make($otpBrut),
-        FieldName::EXPIRE_LE => now()->addMinutes($minutesValidite),
+    public static function generer_otp(User $utilisateur, int $minutesValidite = 10): int
+    {
+        $otpBrut = random_int(100000, 999999);
+        $utilisateur->update([
+            FieldName::OTP => Hash::make($otpBrut),
+            FieldName::EXPIRE_LE => now()->addMinutes($minutesValidite),
+            ]);
+        return $otpBrut;
+    }
+
+    public static function regenerer_mot_de_passe(User $utilisateur): string
+    {
+        $motDePasseClair = Str::password(8, letters: true, numbers: true, symbols: true);
+
+        $utilisateur->update([
+            FieldName::PASSWORD => Hash::make($motDePasseClair),
+            FieldName::OTP => null,
+            FieldName::EXPIRE_LE => null,
+            FieldName::EMAIL_VERIFIE_LE => now(),
         ]);
-    return $otpBrut;
-}
-
-function regenerer_mot_de_passe(User $utilisateur): string
-{
-    $motDePasseClair = Str::password(8, letters: true, numbers: true, symbols: true);
-
-    $utilisateur->update([
-        FieldName::PASSWORD => Hash::make($motDePasseClair),
-        FieldName::OTP => null,
-        FieldName::EXPIRE_LE => null,
-        FieldName::EMAIL_VERIFIE_LE => now(),
-    ]);
-    return $motDePasseClair;
+        return $motDePasseClair;
+    }
 }
