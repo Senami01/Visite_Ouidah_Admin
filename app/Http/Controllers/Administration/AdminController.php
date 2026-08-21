ac489accee680b3a9b2a87f34afa91c529101b6e
Gilles VLAVONOU <deboradoumatey@gmail.com>
2026-08-21 10:00:37 +0100

    Champs type retirer

--- app/Http/Controllers/Administration/AdminController.php
+++ app/Http/Controllers/Administration/AdminController.php
@@ -25,7 +25,6 @@ class AdminController extends BaseController
         ],
         'champs' => [
             FieldName::STATUT,
-            FieldName::TYPE,
         ]
     ];
     public function index()
